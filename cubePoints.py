d6509ad19aa61e344a81f4c954ee2d480ccf0684
Miroslav Zoricak <zoricak@udesign.sk>
2013-04-02 13:59:41 +0200

    Initial Commit

---
+++ cubePoints.py
@@ -0,0 +1,25 @@
+def cube_points(c, wid):
+    """ Creates a list of points for plotting a cube with plot. (the first 5 points are
+    the bottom square, some sides repeated). """
+    p = []
+    # bottom
+    p.append([c[0] - wid, c[1] - wid, c[2] - wid])
+    p.append([c[0] - wid, c[1] + wid, c[2] - wid])
+    p.append([c[0] + wid, c[1] + wid, c[2] - wid])
+    p.append([c[0] + wid, c[1] - wid, c[2] - wid])
+    p.append([c[0] - wid, c[1] - wid, c[2] - wid])
+    # top
+    p.append([c[0] - wid, c[1] - wid, c[2] + wid])
+    p.append([c[0] - wid, c[1] + wid, c[2] + wid])
+    p.append([c[0] + wid, c[1] + wid, c[2] + wid])
+    p.append([c[0] + wid, c[1] - wid, c[2] + wid])
+    p.append([c[0] - wid, c[1] - wid, c[2] + wid])
+    # vertical sides
+    p.append([c[0] - wid, c[1] - wid, c[2] + wid])
+    p.append([c[0] - wid, c[1] + wid, c[2] + wid])
+    p.append([c[0] - wid, c[1] + wid, c[2] - wid])
+    p.append([c[0] + wid, c[1] + wid, c[2] - wid])
+    p.append([c[0] + wid, c[1] + wid, c[2] + wid])
+    p.append([c[0] + wid, c[1] - wid, c[2] + wid])
+    p.append([c[0] + wid, c[1] - wid, c[2] - wid])
+    return array(p).T
